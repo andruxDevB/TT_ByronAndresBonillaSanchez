d4c20b83bc7092b80fd3afd8315b2b7da4020c18
andruxDevB <andres.ldu@hotmail.com>
2023-12-28 22:33:03 -0500

    Estructura básica

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Surtrek Traveller</h1>
+      <div className="container py-4 px-3 mx-auto">
+        <button className="btn btn-primary">Primary button</button>
+      </div>
+      <script type="module" src="./js/main.js"></script>
+    </>
+  )
+}
+
+export default App
